890bd2fa1c839629f7864b3040cd12fbbcc307c3
Neoon <neo@tiefkuehler-madness.me>
2021-02-28 14:43:31 +0100

    change handler for new config structure

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -22,25 +22,26 @@ func DNSHandler(fqdn string, questionType uint16, sourceAddress net.IP, IPv4 boo
 	}
 	var value string
 	value = ""
-	for k := range config.ConfigMap {
-		if strings.Contains(fqdn, k) {
+
+	//Go through each domain in config file
+	for domain := range config.ConfigMap {
+		if strings.Contains(fqdn, domain) {
 			// Split FQDN into domain and subdomain
-			subdomain := fetch.FetchSubDomainName(fqdn, k)
-			if *(config.Debug) == true {
-				log.Printf("Searching subdomain %s in %s\n", subdomain, k)
-			}
-			// Now find if record exists.
-			rrData := fetch.FetchRR(config.ConfigMap[k])
-			for rrName := range rrData {
-				if rrName == subdomain {
-					// Check if rr type match
-					rrType := fetch.FetchRrType(rrData[rrName])
-					if typeChecker(rrType, questionType) {
-						// Match
-						// Use default value first
-						value = fetch.FetchDefaultValue(rrData[rrName])
-					} else {
-						continue
+			subdomain := fetch.FetchSubDomainName(fqdn, domain)
+			// Doesn't parse bla.bla.example.com but bla.example.com needs fix
+			records := fetch.FetchRR(config.ConfigMap[domain])
+			qtype := dns.TypeToString[questionType]
+			//Check if type is defined
+			if typeMatch, ok := records[qtype]; ok {
+				typeMap, ok := typeMatch.(map[interface{}]interface{})
+				//If the map is empty for that type = no record break up
+				if ok == false { break }
+				//If the map is not empty check for subdomain otherwise check for any
+				if recordMatch, ok := typeMap[subdomain]; ok {
+					value = fetch.FetchDefaultValue(recordMatch)
+				} else {
+					if recordMatch, ok := typeMap["any"]; ok {
+						value = fetch.FetchDefaultValue(recordMatch)
 					}
 				}
 			}
